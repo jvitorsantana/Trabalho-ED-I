a256e3ad360835643f3aad0a6caae62c1a23359b
SamuelTorres51 <samuel.torres28@hotmail.com>
2025-07-03 17:35:07 -0300

    lista_circular sendo feita

--- listas/lista_circular_eventos.h
+++ listas/lista_circular_eventos.h
@@ -1,10 +1,21 @@
-#include "estruturas/struct_evento.h"
+#include "../estruturas/struct_evento.h"
 
 #ifndef LISTA_CICULAR_EVENTOS_H
 #define LISTA_CIRCULAR_EVENTOS_H
 
-typedef struct ListaCircular {
-  struct Evento *prox;
-} ListaCirular;
+typedef struct listaeventos {
+  Evento info;
+  struct listaeventos *prox;
+} ListaEventos;
+
+ListaEventos* inicializar();
+
+ListaEventos* inserir_evento(ListaEventos *lista, const char *nome, const char *data);
+void remove_aux(ListaEventos **lista, ListaEventos *anterior, ListaEventos *atual);
+ListaEventos* remover_evento(ListaEventos *lista, const char *nome);
+void imprimir_lista_circular(ListaEventos *lista);
+void liberar_lista_circular(ListaEventos *lista);
+ListaEventos* buscar_evento(ListaEventos *lista, const char *nome);
+
 
 #endif
--- listas/lista_duplamente_encadeada_participante.h
+++ listas/lista_duplamente_encadeada_participante.h
@@ -1,12 +1,13 @@
+#include "estruturas/struct_participante.h"
+
 #ifndef LISTA_DUPLAMENTE_ENCADEADA_PARTICIPANTE_H
 #define LISTA_DUPLAMENTE_ENCADEADA_PARTICIPANTE_H
 
 typedef struct ListaParticipante {
-  Participante *info;
-  struct Participante *ant;
-  struct Participante *prox;
+  Participante info;
+  struct ListaParticipante *ant;
+  struct ListaParticipante *prox;
 } ListaParticipante;
 
-void 
 
 #endif
